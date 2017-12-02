2787766743f8ec972b19348208407ca66739a2cd
Jan-Jue <jan.juettner@gmail.com>
2017-12-02 15:53:39 +0100

    Improved map

---
+++ Assets/Scripts/PixelPerfect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PixelPerfect : MonoBehaviour 
+{
+	public int multiplier = 1;
+	public float divisor = 1.0f;
+
+	void Start () {
+		Camera camera = gameObject.GetComponent<Camera>();
+		camera.orthographicSize = multiplier * (Screen.height / 48.0f / divisor);
+	}
+}
