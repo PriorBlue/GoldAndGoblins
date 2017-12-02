2eb60a33b1b9170cbf0b8497f1130b13cfabd321
Marcus Ihde <marcusplate@googlemail.com>
2017-12-02 19:52:19 +0100

    Add gold/shop functionality.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,14 +9,68 @@ public class Player : MonoBehaviour
     public SpriteRenderer Body;
     public SpriteRenderer Legs;
 
+    public float Gold = 0;
+
     public float Health = 10f;
     public float HealthMax = 10f;
     public float Attack = 1f;
     public float Defence = 1f;
     public float Speed = 10f;
+    public float Digging = 1f;
 
     private float moveX = 0f;
     private float moveY = 0f;
+
+    private string currentField;
+
+    void Update()
+    {
+        if(string.IsNullOrEmpty(currentField) == false)
+        {
+            if(Input.GetButtonDown("Jump"))
+            {
+                if (currentField == "Speed")
+                {
+                    if (Gold >= Speed - 9f)
+                    {
+                        Gold -= (Speed - 9f);
+                        Speed += 1f;
+                    }
+                }
+                else if (currentField == "Attack")
+                {
+                    if (Gold >= Attack)
+                    {
+                        Gold -= Attack;
+                        Attack += 1f;
+                    }
+                }
+                else if (currentField == "Defence")
+                {
+                    if (Gold >= Defence)
+                    {
+                        Gold -= Defence;
+                        Defence += 1f;
+                    }
+                }
+                else if (currentField == "Health")
+                {
+                    if (Gold >= 1)
+                    {
+                        Gold -= 1;
+                        Health = 10f;
+                    }
+                }
+            }
+            else if(Input.GetButton("Jump"))
+            {
+                if (currentField == "Gold")
+                {
+                    Gold += Digging * Time.deltaTime;
+                }
+            }
+        }
+    }
 
     void FixedUpdate()
     {
@@ -27,5 +81,15 @@ public class Player : MonoBehaviour
         moveY = Mathf.Lerp(moveY, dy, Time.fixedDeltaTime * Speed * 2f);
 
         rig.velocity = new Vector2(moveX * Speed, moveY * Speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentField = collision.tag;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        currentField = null;
     }
 }
